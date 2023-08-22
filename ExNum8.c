42fba45bec9e0c14e595b4066a321ae21f033875
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-08-22 11:18:48 -0300

    Update ExNum8.c

--- ExNum8.c
+++ ExNum8.c
@@ -2,6 +2,19 @@
 #include<math.h>
 
 main(){
+    
+    float salarioVelho, percReajuste, salarioNovo;
+
+    printf("Digite seu salario: ");
+    scanf("%f", &salarioVelho);
+
+    printf("Digite o percentual de reajuste: ");
+    scanf("%f", &percReajuste);
+
+    salarioNovo = salarioVelho + (salarioVelho * (percReajuste / 100));
+
+    printf("O salario novo e de %.2f reais", salarioNovo);
+
 
 
 
