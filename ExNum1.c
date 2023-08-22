f4f0c1a330499ba357cc1d4d792c145fd5f266a7
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-08-22 11:09:10 -0300

    Add files via upload

--- ExNum1.c
+++ ExNum1.c
@@ -4,8 +4,28 @@
 main(){
 
     int num1, num2, soma, sub, mult;
-    float div
+    float div;
 
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+
+    printf("\nDigite o segundo numero: ");
+    scanf("%d", &num2);
+
+    //operacoes
+
+    soma = num1 + num2;
+    sub = num1 - num2;
+    mult = num1 * num2;
+    
+    div = (float)num1 / (float)num2;
+
+    //saida
+
+    printf("\nSoma: %d", soma);
+    printf("\nSubtracao: %d", sub);
+    printf("\nMultiplicacao: %d", mult);
+    printf("\nDivisao: %f", div);
 
 
 }
