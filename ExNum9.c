017ab95f210ba014fce3d6cb3a04a05b310bbe83
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-08-22 11:24:29 -0300

    Update ExNum9.c

--- ExNum9.c
+++ ExNum9.c
@@ -3,6 +3,14 @@
 
 main(){
 
+    float custoFabrica, custoConsumidor;
+
+    printf("Digite o custo de fabrica: ");
+    scanf("%f", &custoFabrica);
+
+    custoConsumidor = custoFabrica + (custoFabrica * 0.28) + (custoFabrica * 0.45);
+
+    printf("O custo final ao consumidor e de %.2f reais", custoConsumidor);
 
 
     
