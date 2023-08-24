8d541afde8b2ecc028e5189e998006e71a077820
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-08-24 09:06:30 -0300

    Atualizar o ExNum10.c

--- ExNum10.c
+++ ExNum10.c
@@ -2,8 +2,29 @@
 #include<math.h>
 
 main(){
+    
+    float salarioFixo, comissaoVendas, valorVendas, salarioVendas, salarioCarros, salarioFinal;
+    int carrosVendidos;
+    
+    printf("\nDigite seu salario fixo");
+    scanf("%f", &salarioFixo);
+    
+    printf("\nDigite a comissao por carro vendido");
+    scanf("%f", &comissaoVendas);
 
-
+    printf("\nDigite o numero de carros vendidos");
+    scanf("%f", &carrosVendidos);
+    
+    printf("\nDigite o valor total de suas vendas");
+    scanf("%f", &valorVendas);
+    
+    salarioVendas = valorVendas + (valorVendas * 0,05);
+    
+    salarioCarros = carrosVendidos * comissaoVendas;
+    
+    salarioFinal = salarioFixo + salarioVendas + salarioCarros;
+    
+    printf("\nSeu salario final e %f", salarioFinal);
 
     
 }
